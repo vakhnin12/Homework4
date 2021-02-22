687200d205f579dee0ea1b86c63653b45eb6f77d
Serhii <70597660+vakhnin12@users.noreply.github.com>
2021-02-22 18:36:41 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,16 +1,13 @@
 const d = +prompt("Input Diameter of circle in mm");
-let square = "";
-let length = "";
 
-function getCircleSquare() {
-    let rad = d / 2;
-    square = 3.14 * (rad ** 2);
+function getCircleSquare(diameter) {
+    let rad = diameter / 2;
+    return Math.PI * (rad ** 2);
 }
-getCircleSquare();
-console.log("Square of a circle = " + square + "mm^2");
 
-function getCircleLength() {
-    length = 3.14 * d;
+function getCircleLength(diameter) {
+    return Math.PI * diameter;
 }
-getCircleLength()
-console.log("Length of a circle = " + length + "mm");
+
+console.log("Square of a circle = " + getCircleSquare(d) + "mm^2");
+console.log("Length of a circle = " + getCircleLength(d) + "mm");
